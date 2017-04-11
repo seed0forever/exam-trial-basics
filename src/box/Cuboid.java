9b116d88a54c61caeb49ca49e557a2da8232d37c
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-04-11 16:23:53 +0200

    task4 'Box' add default constructor

--- src/box/Cuboid.java
+++ src/box/Cuboid.java
@@ -5,4 +5,12 @@ public class Cuboid {
   // It should take its three dimensions as constructor parameters (numbers)
   // It should have a method called `getSurface` that returns the cuboid's surface
   // It should have a method called `getVolume` that returns the cuboid's volume
+
+  float width, height, depth;
+
+  public Cuboid(float width, float height, float depth) {
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
+  }
 }
